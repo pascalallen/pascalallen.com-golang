886b600f5c511263da69714c89937780c62f54f3
Pascal Allen <pascal@onlinemeded.org>
2019-07-30 17:27:05 -0500

    rm dep, use go get instead.

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # React | MySQL | Go -- Web App
 
-Requires Go, Dep, and Node to be installed.
-
 `git fetch && git checkout https://github.com/pascalallen/pascalallen.com-golang.git`
 
-`dep init && dep ensure`
+`go get && go install && go run main.go`
--- main.go
+++ main.go
@@ -1,66 +1,78 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "database/sql"
-    "net/http"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
 )
 
 func main() {
-    fmt.Println("Go MySQL Tutorial")
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello Mars!")
+	}))
 
-    http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        log.Println("Hello Mars!")
-    }))
+	http.ListenAndServe(":3000", nil)
+	log.Println("Server is now running on port 3000")
 
-    http.ListenAndServe(":3000", nil)
-    log.Println("Server is now running on port 3000")
+	type Post struct {
+		Id    string `json:"id"`
+		Title string `json:"title"`
+		Body  string `json:"body"`
+	}
 
-    type Post struct {
-        Id string `json:"id"`
-        Title string `json:"title"`
-        Body string `json:"body"`
-    }
+	db, err := sql.Open("mysql", "homestead:secret@tcp(127.0.0.1:3306)/pascalallen.com")
 
-    db, err := sql.Open("mysql", "homestead:secret@tcp(127.0.0.1:3306)/pascalallen.com")
+	if err != nil {
+		panic(err.Error())
+	}
 
-    if err != nil {
-        panic(err.Error())
-    }
+	defer db.Close()
 
-    defer db.Close()
+	results, err := db.Query("SELECT * FROM posts")
+	if err != nil {
+		panic(err.Error())
+	}
 
-    results, err := db.Query("SELECT * FROM posts")
-    if err != nil {
-        panic(err.Error())
-    }
+	for results.Next() {
+		var post Post
+		err = results.Scan(&post.Id, &post.Title, &post.Body)
+		if err != nil {
+			panic(err.Error())
+		}
+		log.Printf(post.Title)
+	}
 
-    for results.Next() {
-        var post Post
-        err = results.Scan(&post.Id, &post.Title, &post.Body)
-        if err != nil {
-            panic(err.Error())
-        }
-        log.Printf(post.Title)
-    }
+	{
+		{ /*    insert, err := db.Query("INSERT INTO posts VALUES ('title', 'body')")*/
+		}
+	}
 
-{{/*    insert, err := db.Query("INSERT INTO posts VALUES ('title', 'body')")*/}}
+	{
+		{ /*    if err != nil {*/
+		}
+	}
+	{
+		{ /*        panic(err.Error())*/
+		}
+	}
+	{
+		{ /*    }*/
+		}
+	}
 
-{{/*    if err != nil {*/}}
-{{/*        panic(err.Error())*/}}
-{{/*    }*/}}
+	{
+		{ /*    defer insert.Close()*/
+		}
+	}
 
-{{/*    defer insert.Close()*/}}
+	var post Post
+	err = db.QueryRow("SELECT * FROM posts where id = ?", 1).Scan(&post.Id, &post.Title, &post.Body)
+	if err != nil {
+		panic(err.Error())
+	}
 
-    var post Post
-    err = db.QueryRow("SELECT * FROM posts where id = ?", 1).Scan(&post.Id, &post.Title, &post.Body)
-    if err != nil {
-        panic(err.Error())
-    }
-
-    log.Println(post.Id)
-    log.Println(post.Title)
-    log.Println(post.Body)
+	log.Println(post.Id)
+	log.Println(post.Title)
+	log.Println(post.Body)
 }
