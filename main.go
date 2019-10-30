69772590bfa2fc301ff9b9f75a5f897bd6e708ce
Pascal Allen <pascal@onlinemeded.org>
2019-10-29 20:49:26 -0500

    init db

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pascalallen/auth"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting server on port 8080...")
+	http.HandleFunc("/login", auth.Login)
+	http.HandleFunc("/register", auth.Register)
+	http.ListenAndServe(":8080", nil)
+}
