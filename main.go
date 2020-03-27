0cf886202165a944d4c69548a1fa9464c01579c2
Pascal Allen <pascal@onlinemeded.org>
2020-03-27 15:04:31 -0500

    update struct

--- main.go
+++ main.go
@@ -1,78 +1,16 @@
 package main
 
 import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
+	"fmt"
+	"github.com/pascalallen/auth"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello Mars!")
-	}))
+	fmt.Println("Starting server on port 8080...")
 
-	http.ListenAndServe(":3000", nil)
-	log.Println("Server is now running on port 3000")
+	http.HandleFunc("/login", auth.Login)
+	http.HandleFunc("/register", auth.Register)
 
-	type Post struct {
-		Id    string `json:"id"`
-		Title string `json:"title"`
-		Body  string `json:"body"`
-	}
-
-	db, err := sql.Open("mysql", "homestead:secret@tcp(127.0.0.1:3306)/pascalallen.com")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer db.Close()
-
-	results, err := db.Query("SELECT * FROM posts")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for results.Next() {
-		var post Post
-		err = results.Scan(&post.Id, &post.Title, &post.Body)
-		if err != nil {
-			panic(err.Error())
-		}
-		log.Printf(post.Title)
-	}
-
-	{
-		{ /*    insert, err := db.Query("INSERT INTO posts VALUES ('title', 'body')")*/
-		}
-	}
-
-	{
-		{ /*    if err != nil {*/
-		}
-	}
-	{
-		{ /*        panic(err.Error())*/
-		}
-	}
-	{
-		{ /*    }*/
-		}
-	}
-
-	{
-		{ /*    defer insert.Close()*/
-		}
-	}
-
-	var post Post
-	err = db.QueryRow("SELECT * FROM posts where id = ?", 1).Scan(&post.Id, &post.Title, &post.Body)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	log.Println(post.Id)
-	log.Println(post.Title)
-	log.Println(post.Body)
+	http.ListenAndServe(":8080", nil)
 }
